062ed8f9689effa52d72a20ed6f6b9a80378f99f
soih87 <122946915+soih87@users.noreply.github.com>
2023-02-02 14:37:33 +0900

    ERD final

--- 기본프로젝트/SilverScore_DDL.sql
+++ 기본프로젝트/SilverScore_DDL.sql
@@ -1,5 +1,5 @@
 /* 회원 */
-CREATE TABLE member (
+CREATE TABLE Member (
 	id number(5) NOT NULL, /* 회원id */
 	name varchar(20) NOT NULL, /* 이름 */
 	passwd varchar(15) NOT NULL, /* 비밀번호 */
@@ -8,31 +8,31 @@ CREATE TABLE member (
 	registerDate date NOT NULL /* 가입일 */
 );
 
-CREATE UNIQUE INDEX PK_member
-	ON member (
+CREATE UNIQUE INDEX PK_Member
+	ON Member (
 		id ASC
 	);
 
-ALTER TABLE member
+ALTER TABLE Member
 	ADD
-		CONSTRAINT PK_member
+		CONSTRAINT PK_Member
 		PRIMARY KEY (
 			id
 		);
 
-COMMENT ON TABLE member IS '회원';
+COMMENT ON TABLE Member IS '회원';
 
-COMMENT ON COLUMN member.id IS '회원id';
+COMMENT ON COLUMN Member.id IS '회원id';
 
-COMMENT ON COLUMN member.name IS '이름';
+COMMENT ON COLUMN Member.name IS '이름';
 
-COMMENT ON COLUMN member.passwd IS '비밀번호';
+COMMENT ON COLUMN Member.passwd IS '비밀번호';
 
-COMMENT ON COLUMN member.phone IS '핸드폰';
+COMMENT ON COLUMN Member.phone IS '핸드폰';
 
-COMMENT ON COLUMN member.email IS '이메일';
+COMMENT ON COLUMN Member.email IS '이메일';
 
-COMMENT ON COLUMN member.registerDate IS '가입일';
+COMMENT ON COLUMN Member.registerDate IS '가입일';
 
 /* 요양기관 */
 CREATE TABLE LongTermAdmin (
@@ -103,11 +103,11 @@ COMMENT ON COLUMN LongTermAdmin.stpRptDt IS '설치신고일';
 COMMENT ON COLUMN LongTermAdmin.adminCd IS '기관구분코드';
 
 /* 리뷰 */
-CREATE TABLE review (
+CREATE TABLE Review (
 	reviewId number(5) NOT NULL, /* 리뷰id */
 	amKind number(1) NOT NULL, /* 분위기/친절도 */
 	faClean number(1) NOT NULL, /* 시설/청결도 */
-	content varchar(500), /* 내용 */
+	content varchar(2000), /* 내용 */
 	createDate date NOT NULL, /* 작성일 */
 	modifyDate date, /* 수정일 */
 	id number(5) NOT NULL, /* 회원id */
@@ -115,39 +115,39 @@ CREATE TABLE review (
 	adminPttnCd char(3) NOT NULL /* 기관유형코드 */
 );
 
-CREATE UNIQUE INDEX PK_review
-	ON review (
+CREATE UNIQUE INDEX PK_Review
+	ON Review (
 		reviewId ASC
 	);
 
-ALTER TABLE review
+ALTER TABLE Review
 	ADD
-		CONSTRAINT PK_review
+		CONSTRAINT PK_Review
 		PRIMARY KEY (
 			reviewId
 		);
 
-COMMENT ON TABLE review IS '리뷰';
+COMMENT ON TABLE Review IS '리뷰';
 
-COMMENT ON COLUMN review.reviewId IS '리뷰id';
+COMMENT ON COLUMN Review.reviewId IS '리뷰id';
 
-COMMENT ON COLUMN review.amKind IS '분위기/친절도';
+COMMENT ON COLUMN Review.amKind IS '분위기/친절도';
 
-COMMENT ON COLUMN review.faClean IS '시설/청결도';
+COMMENT ON COLUMN Review.faClean IS '시설/청결도';
 
-COMMENT ON COLUMN review.content IS '내용';
+COMMENT ON COLUMN Review.content IS '내용';
 
-COMMENT ON COLUMN review.createDate IS '작성일';
+COMMENT ON COLUMN Review.createDate IS '작성일';
 
-COMMENT ON COLUMN review.modifyDate IS '수정일';
+COMMENT ON COLUMN Review.modifyDate IS '수정일';
 
-COMMENT ON COLUMN review.id IS '회원id';
+COMMENT ON COLUMN Review.id IS '회원id';
 
-COMMENT ON COLUMN review.longTermAdminSym IS '기관기호';
+COMMENT ON COLUMN Review.longTermAdminSym IS '기관기호';
 
-COMMENT ON COLUMN review.adminPttnCd IS '기관유형코드';
+COMMENT ON COLUMN Review.adminPttnCd IS '기관유형코드';
 
-/* 공공_요양기관_평가등급 */
+/* 요양기관평가등급 */
 CREATE TABLE LongTermAdminRating (
 	no char(3) NOT NULL, /* 연번 */
 	ratingCd char(15), /* 평가구분 */
@@ -160,7 +160,7 @@ CREATE TABLE LongTermAdminRating (
 	ratingGrade char(1), /* 평가등급 */
 	opRating number(3), /* 기관운영 */
 	safeRating number(3), /* 환경및안전 */
-	rightRating number(3), /* 수급자권리보장 */
+	rightRating  number(3), /* 수급자권리보장 */
 	processRating number(3), /* 급여제공과정 */
 	resultRating number(3) /* 급여제공결과 */
 );
@@ -177,7 +177,7 @@ ALTER TABLE LongTermAdminRating
 			no
 		);
 
-COMMENT ON TABLE LongTermAdminRating IS '공공_요양기관_평가등급';
+COMMENT ON TABLE LongTermAdminRating IS '요양기관평가등급';
 
 COMMENT ON COLUMN LongTermAdminRating.no IS '연번';
 
@@ -201,13 +201,13 @@ COMMENT ON COLUMN LongTermAdminRating.opRating IS '기관운영';
 
 COMMENT ON COLUMN LongTermAdminRating.safeRating IS '환경및안전';
 
-COMMENT ON COLUMN LongTermAdminRating.rightRating IS '수급자권리보장';
+COMMENT ON COLUMN LongTermAdminRating.rightRating  IS '수급자권리보장';
 
 COMMENT ON COLUMN LongTermAdminRating.processRating IS '급여제공과정';
 
 COMMENT ON COLUMN LongTermAdminRating.resultRating IS '급여제공결과';
 
-/* 기관기타현황 */
+/* 기관기타현황상세 */
 CREATE TABLE InsttEtcDetail (
 	longTermAdminSym char(11) NOT NULL, /* 기관기호 */
 	adminPttnCd char(3) NOT NULL, /* 기관유형코드 */
@@ -230,7 +230,7 @@ ALTER TABLE InsttEtcDetail
 			adminPttnCd
 		);
 
-COMMENT ON TABLE InsttEtcDetail IS '기관기타현황';
+COMMENT ON TABLE InsttEtcDetail IS '기관기타현황상세';
 
 COMMENT ON COLUMN InsttEtcDetail.longTermAdminSym IS '기관기호';
 
@@ -243,30 +243,30 @@ COMMENT ON COLUMN InsttEtcDetail.tfMth IS '교통편';
 COMMENT ON COLUMN InsttEtcDetail.pkngEquip IS '주차시설';
 
 /* 기관유형코드 */
-CREATE TABLE TABLE6 (
+CREATE TABLE adminPttnCode (
 	adminPttnCd char(3) NOT NULL, /* 기관유형코드 */
 	adminPttnNm varchar(40) NOT NULL /* 기관유형이름 */
 );
 
-CREATE UNIQUE INDEX PK_TABLE6
-	ON TABLE6 (
+CREATE UNIQUE INDEX PK_adminPttnCode
+	ON adminPttnCode (
 		adminPttnCd ASC
 	);
 
-ALTER TABLE TABLE6
+ALTER TABLE adminPttnCode
 	ADD
-		CONSTRAINT PK_TABLE6
+		CONSTRAINT PK_adminPttnCode
 		PRIMARY KEY (
 			adminPttnCd
 		);
 
-COMMENT ON TABLE TABLE6 IS '기관유형코드';
+COMMENT ON TABLE adminPttnCode IS '기관유형코드';
 
-COMMENT ON COLUMN TABLE6.adminPttnCd IS '기관유형코드';
+COMMENT ON COLUMN adminPttnCode.adminPttnCd IS '기관유형코드';
 
-COMMENT ON COLUMN TABLE6.adminPttnNm IS '기관유형이름';
+COMMENT ON COLUMN adminPttnCode.adminPttnNm IS '기관유형이름';
 
-/* 일반현황 */
+/* 일반현황상세 */
 CREATE TABLE GeneralSttusDetail (
 	longTermAdminSym char(11) NOT NULL, /* 기관기호 */
 	adminPttnCd char(3) NOT NULL, /* 기관유형코드 */
@@ -303,7 +303,7 @@ ALTER TABLE GeneralSttusDetail
 			adminPttnCd
 		);
 
-COMMENT ON TABLE GeneralSttusDetail IS '일반현황';
+COMMENT ON TABLE GeneralSttusDetail IS '일반현황상세';
 
 COMMENT ON COLUMN GeneralSttusDetail.longTermAdminSym IS '기관기호';
 
@@ -343,7 +343,7 @@ COMMENT ON COLUMN GeneralSttusDetail.longTermPeribRgtDt IS '장기요양기관지정일';
 
 COMMENT ON COLUMN GeneralSttusDetail.StpRptDt IS '설치신고일자';
 
-/* 시설현황 */
+/* 시설현황상세 */
 CREATE TABLE InsttSttusDetail (
 	longTermAdminSym char(11) NOT NULL, /* 기관기호 */
 	adminPttnCd char(3) NOT NULL, /* 기관유형코드 */
@@ -356,7 +356,7 @@ CREATE TABLE InsttSttusDetail (
 	medRoomReal number(3) NOT NULL, /* 의료및간호사실 */
 	funcTrnRoomReal number(3) NOT NULL, /* 작업및일상동작훈련실 */
 	pgmRoomReal number(3) NOT NULL, /* 프로그램실 */
-	crmnyPrst number(3) NOT NULL, /* 식당및조리실 */
+	crmnyPrst number(3) NOT NULL, /* 식당_조리실 */
 	batRoom number(3) NOT NULL, /* 화장실 */
 	taxPageLong number(3) NOT NULL, /* 세면장및목욕실 */
 	taxRoom number(3) NOT NULL /* 세탁장및건조장 */
@@ -376,7 +376,7 @@ ALTER TABLE InsttSttusDetail
 			adminPttnCd
 		);
 
-COMMENT ON TABLE InsttSttusDetail IS '시설현황';
+COMMENT ON TABLE InsttSttusDetail IS '시설현황상세';
 
 COMMENT ON COLUMN InsttSttusDetail.longTermAdminSym IS '기관기호';
 
@@ -400,7 +400,7 @@ COMMENT ON COLUMN InsttSttusDetail.funcTrnRoomReal IS '작업및일상동작훈련실';
 
 COMMENT ON COLUMN InsttSttusDetail.pgmRoomReal IS '프로그램실';
 
-COMMENT ON COLUMN InsttSttusDetail.crmnyPrst IS '식당및조리실';
+COMMENT ON COLUMN InsttSttusDetail.crmnyPrst IS '식당_조리실';
 
 COMMENT ON COLUMN InsttSttusDetail.batRoom IS '화장실';
 
@@ -408,7 +408,7 @@ COMMENT ON COLUMN InsttSttusDetail.taxPageLong IS '세면장및목욕실';
 
 COMMENT ON COLUMN InsttSttusDetail.taxRoom IS '세탁장및건조장';
 
-/* 입소인원현황 */
+/* 입소인원현황상세 */
 CREATE TABLE AceptncNmprDetail (
 	longTermAdminSym char(11) NOT NULL, /* 기관기호 */
 	adminPttnCd char(3) NOT NULL, /* 기관유형코드 */
@@ -433,7 +433,7 @@ ALTER TABLE AceptncNmprDetail
 			adminPttnCd
 		);
 
-COMMENT ON TABLE AceptncNmprDetail IS '입소인원현황';
+COMMENT ON TABLE AceptncNmprDetail IS '입소인원현황상세';
 
 COMMENT ON COLUMN AceptncNmprDetail.longTermAdminSym IS '기관기호';
 
@@ -449,97 +449,73 @@ COMMENT ON COLUMN AceptncNmprDetail.maRsvPer IS '대기_남';
 
 COMMENT ON COLUMN AceptncNmprDetail.fmRsvPer IS '대기_여';
 
-/* 급여종류 */
-CREATE TABLE TABLE10 (
-	COL char(1) NOT NULL, /* 급여종별코드 */
-	COL2 varchar(20) NOT NULL /* 급여종류 */
-);
-
-CREATE UNIQUE INDEX PK_TABLE10
-	ON TABLE10 (
-		COL ASC
-	);
-
-ALTER TABLE TABLE10
-	ADD
-		CONSTRAINT PK_TABLE10
-		PRIMARY KEY (
-			COL
-		);
-
-COMMENT ON TABLE TABLE10 IS '급여종류';
-
-COMMENT ON COLUMN TABLE10.COL IS '급여종별코드';
-
-COMMENT ON COLUMN TABLE10.COL2 IS '급여종류';
-
-/* 지역코드 */
-CREATE TABLE locationCd (
+/* 시도군구코드 */
+CREATE TABLE siDoGunGuCode (
 	siDoCd char(2) NOT NULL, /* 시도코드 */
 	siGunGuCd char(3) NOT NULL, /* 시군구코드 */
 	siDoNm varchar(20), /* 시도명 */
 	siGunGuNm varchar(20) /* 시군구명 */
 );
 
-CREATE UNIQUE INDEX PK_locationCd
-	ON locationCd (
+CREATE UNIQUE INDEX PK_siDoGunGuCode
+	ON siDoGunGuCode (
 		siDoCd ASC,
 		siGunGuCd ASC
 	);
 
-ALTER TABLE locationCd
+ALTER TABLE siDoGunGuCode
 	ADD
-		CONSTRAINT PK_locationCd
+		CONSTRAINT PK_siDoGunGuCode
 		PRIMARY KEY (
 			siDoCd,
 			siGunGuCd
 		);
 
-COMMENT ON TABLE locationCd IS '지역코드';
+COMMENT ON TABLE siDoGunGuCode IS '시도군구코드';
 
-COMMENT ON COLUMN locationCd.siDoCd IS '시도코드';
+COMMENT ON COLUMN siDoGunGuCode.siDoCd IS '시도코드';
 
-COMMENT ON COLUMN locationCd.siGunGuCd IS '시군구코드';
+COMMENT ON COLUMN siDoGunGuCode.siGunGuCd IS '시군구코드';
 
-COMMENT ON COLUMN locationCd.siDoNm IS '시도명';
+COMMENT ON COLUMN siDoGunGuCode.siDoNm IS '시도명';
 
-COMMENT ON COLUMN locationCd.siGunGuNm IS '시군구명';
+COMMENT ON COLUMN siDoGunGuCode.siGunGuNm IS '시군구명';
 
 ALTER TABLE LongTermAdmin
 	ADD
-		CONSTRAINT FK_TABLE6_TO_LongTermAdmin
+		CONSTRAINT FK_adminPttnCode_TO_LongTermAdmin
 		FOREIGN KEY (
 			adminPttnCd
 		)
-		REFERENCES TABLE6 (
+		REFERENCES adminPttnCode (
 			adminPttnCd
 		);
 
 ALTER TABLE LongTermAdmin
 	ADD
-		CONSTRAINT FK_locationCd_TO_LongTermAdmin
+		CONSTRAINT FK_siDoGunGuCode_TO_LongTermAdmin
 		FOREIGN KEY (
 			siDoCd,
 			siGunGuCd
 		)
-		REFERENCES locationCd (
+		REFERENCES siDoGunGuCode (
 			siDoCd,
 			siGunGuCd
 		);
 
-ALTER TABLE review
+ALTER TABLE Review
 	ADD
-		CONSTRAINT FK_member_TO_review
+		CONSTRAINT FK_Member_TO_Review
 		FOREIGN KEY (
 			id
 		)
-		REFERENCES member (
+		REFERENCES Member (
 			id
 		);
 
-ALTER TABLE review
+ALTER TABLE Review
 	ADD
-		CONSTRAINT FK_LongTermAdmin_TO_review
+		CONSTRAINT FK_LongTermAdmin_TO_Review
 		FOREIGN KEY (
 			longTermAdminSym,
 			adminPttnCd
